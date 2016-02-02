159ef030c3382abeeea98fdc50a12c364fbc2c6f
robotonics <roboshopz@gmail.com>
2016-02-02 20:22:52 +0000

    First Commit of  files

---
+++ robotapp/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using IoTHelpers.Gpio.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace robotapp
+{
+    public sealed partial class MainPage : Page
+    {
+        private Sr04UltrasonicDistanceSensor ultrasonic;
+        public object dist { get; private set; }
+
+        public MainPage()
+
+        {
+            this.InitializeComponent();
+            Unloaded += MainPage_Unloaded;
+            ultrasonic = new Sr04UltrasonicDistanceSensor(triggerPinNumber: 12, echoPinNumber: 16);
+            ultrasonic.DistanceChanged += ultrasonic_DistanceChanged;
+            
+            
+            
+            
+        }
+
+        private  void sonar()
+        {
+            sonardata.Text = dist.ToString();
+
+        }
+
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+
+        {
+            if (ultrasonic != null)
+
+            {
+               
+                ultrasonic.DistanceChanged -= ultrasonic_DistanceChanged;
+                 ultrasonic.Dispose();
+                sonar();
+               
+            }
+        }
+
+        private void ultrasonic_DistanceChanged(object sender, EventArgs e)
+
+        {
+            dist = ultrasonic.CurrentDistance;
+                             
+
+        }
+
+        
+    }
+
+}
